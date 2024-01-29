a852b7d8ab71fd62cac35411be7b9cc2851be80c
Tomilola <83653154+Ayolola03@users.noreply.github.com>
2024-01-29 16:40:13 +0100

    little changes to mongo url

--- telegram_bot/index.js
+++ telegram_bot/index.js
@@ -6,13 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 3000; // Set the port
 
 // Set up mongoose connection
-mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://Blue:gdsc1234@cluster0.yrqqhrt.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url =
+  "mongodb+srv://Blue:gdsc1234@cluster0.yrqqhrt.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+
 
 // Middleware
 app.use(bodyParser.json());
